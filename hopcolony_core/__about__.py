a91d663dde91c12b9ba0261d73ad3131e653128a
Luis Paarup <lpaarup@gmail.com>
2021-02-23 22:14:31 +0100

    0.1.2

--- hopcolony_core/__about__.py
+++ hopcolony_core/__about__.py
@@ -1,6 +1,6 @@
 """About information for Hop Colony python SDK."""
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __title__ = 'hopcolony_core'
 __author__ = 'HopColony'
 __author_email__ = 'hopcolony@gmail.com'
